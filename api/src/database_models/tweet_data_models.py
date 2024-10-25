4714eabc4148b4e6ea726f4e633faf8ed094c1e4
tony <technicalprogram4658@gmail.com>
2024-10-25 20:22:12 +0300

    Add relations and foreign keys to db tables Fix Cookies.expiration_date filed type (now TIMESTAMP) Fix alembic migration

--- api/src/database_models/tweet_data_models.py
+++ api/src/database_models/tweet_data_models.py
@@ -1,20 +1,25 @@
-from typing import List, Optional, Dict
+import asyncio
+from typing import List
 
-from sqlalchemy import select, delete, update
+from sqlalchemy import select, delete, update, ForeignKey, UniqueConstraint
 from sqlalchemy.types import VARCHAR
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db_config import BaseModel, base_metadata
+from database_models.users_models import Users
+from db_config import BaseModel, base_metadata, engine, async_session
 
 
 class Tweets(BaseModel):
     __tablename__ = "tweets"
     metadata = base_metadata
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True,)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     data: Mapped[str] = mapped_column(VARCHAR(1000), nullable=False)
+
+    medias: Mapped[List["Medias"]] = relationship(back_populates="tweet", uselist=True, lazy="joined")
+    user: Mapped["Users"] = relationship(back_populates="tweets")
 
 
 class Medias(BaseModel):
@@ -22,14 +27,17 @@ class Medias(BaseModel):
     metadata = base_metadata
 
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
-    tweet_id: Mapped[int] = mapped_column(nullable=False)
+    tweet_id: Mapped[int] = mapped_column(ForeignKey("tweets.id",), nullable=False)
     data: Mapped[str] = mapped_column(nullable=False)  # непонятно как хранить медиа
+
+    tweet: Mapped["Tweets"] = relationship(back_populates="medias")
 
 
 class Likes(BaseModel):
     __tablename__ = "likes"
     metadata = base_metadata
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(nullable=False)
-    tweet_id: Mapped[int] = mapped_column(nullable=False)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False, primary_key=True)
+    tweet_id: Mapped[int] = mapped_column(ForeignKey("tweets.id"), nullable=False, primary_key=True)
+
+    __table_args__ = (UniqueConstraint("user_id", "tweet_id", name="unique_like"),)
--- api/src/database_models/users_models.py
+++ api/src/database_models/users_models.py
@@ -1,9 +1,9 @@
 from typing import List, Optional, Dict
 from datetime import datetime, timedelta
 
-from sqlalchemy import select, delete
-from sqlalchemy.types import VARCHAR, DATETIME
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy import select, delete, ForeignKey, UniqueConstraint
+from sqlalchemy.types import VARCHAR, TIMESTAMP
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db_config import BaseModel, base_metadata
@@ -13,35 +13,55 @@ class Users(BaseModel):
     __tablename__ = "users"
     metadata = base_metadata
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
-    username: Mapped[str] = mapped_column(VARCHAR(15), unique=True, nullable=False)
-    email: Mapped[str] = mapped_column(VARCHAR(30), unique=True, nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True,)
+    username: Mapped[str] = mapped_column(VARCHAR(15), unique=True, nullable=False,)
+    email: Mapped[str] = mapped_column(VARCHAR(30), unique=True, nullable=False,)
 
-    @classmethod
-    async def basic_template(cls, session: AsyncSession) -> Dict:
-        async with session as session:
-             data = await session.get()
-        return ...
+    tweets: Mapped[List["Tweets"]] = relationship(back_populates="user", uselist=True, lazy="joined")
+
+    followers: Mapped[List["Followers"]] = relationship(back_populates="user",
+                                                        foreign_keys='Followers.user_id',
+                                                        uselist=True,
+                                                        lazy="joined",
+                                                        )
+    following: Mapped[List["Followers"]] = relationship(back_populates="follower",
+                                                        foreign_keys='Followers.follower_id',
+                                                        uselist=True,
+                                                        lazy="joined",
+                                                        )
 
 
 class Followers(BaseModel):
     __tablename__ = "followers"
     metadata = base_metadata
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(nullable=False)
-    follower_id: Mapped[int] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False,)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False,)
+    follower_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=False,)
+
+    user: Mapped["Users"] = relationship(foreign_keys=[user_id],
+                                         back_populates='followers',
+                                         )
+    follower: Mapped["Users"] = relationship(foreign_keys=[follower_id],
+                                             back_populates='following',
+                                             )
+
+    __table_args__ = (UniqueConstraint("user_id",
+                                       "follower_id",
+                                       name="unique_follow",
+                                       ),
+                      )
 
 
 class Cookies(BaseModel):
     __tablename__ = "cookies"
     metadata = base_metadata
 
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True,)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     hash: Mapped[str] = mapped_column(VARCHAR(100), unique=True, nullable=False)
     expiration_date: Mapped[datetime] = mapped_column(
-        DATETIME,
+        TIMESTAMP,
         nullable=False,
         default=datetime.now() + timedelta(7),
     )
