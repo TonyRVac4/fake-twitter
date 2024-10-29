a0e996538a1d8f41e664ee55a2fe52f18cddc7a0
tony <technicalprogram4658@gmail.com>
2024-10-29 19:28:38 +0300

    Add: new response message in case data weren't found

--- api/src/database_models/methods/tweets.py
+++ api/src/database_models/methods/tweets.py
@@ -10,14 +10,23 @@ from database_models.db_config import BaseModel, base_metadata, engine, async_se
 
 class TweetsMethods(Tweets):
     @classmethod
-    async def get_all(cls, async_session: AsyncSession, user_id) -> dict:
+    async def get_all(cls, user_id, async_session: AsyncSession) -> dict:
         async with async_session as session:
             expression = select(Tweets).where(Tweets.user_id == user_id)
+
             request = await session.execute(expression)
+            request_data: list = request.scalars().fetchall()
+
+            if not request_data:
+                return {
+                    "result": False,
+                    "error_type": "DataNotFound",
+                    "error_message": "User with id:{id} not found".format(id=user_id),
+                }
 
             result: dict = {"result": True, "tweet": []}
 
-            for res in request.scalars().fetchall():
+            for res in request_data:
                 author: dict = {"id": res.user.id, "name": res.user.username}
                 attachments: List[str] = [media.data for media in res.medias]
                 likes: List[dict] = [{"user_id": like.user_id, "name": like.user.username} for like in res.likes]
