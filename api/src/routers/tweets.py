d4697ff210661d2c02c754debb80baa04ae7bcfa
tony <technicalprogram4658@gmail.com>
2024-10-30 23:00:30 +0300

    Add logic for POST & DELETE "/api/tweets" endpoints

--- api/src/routers/tweets.py
+++ api/src/routers/tweets.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends, Request  # HTTPException
+from fastapi import APIRouter, status, Depends, Request
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.responses import JSONResponse
@@ -6,7 +6,7 @@ from schemas import BaseResponseDataOut, TweetDataIn, TweetResponseWithId
 
 from database_models.methods.tweets import TweetsMethods
 from database_models.methods.users import CookiesMethods
-from database_models.db_config import get_async_session
+from database_models.db_config import get_async_session, ResponseData
 
 router = APIRouter(
     prefix="/api/tweets",
@@ -28,27 +28,22 @@ async def posts_list(request: Request, session: AsyncSession = Depends(get_async
         JSON: результат запроса и список словорей с постами.
     """
 
-    check_api_key: dict = await CookiesMethods.get_user_id(request.headers.get("api-key"), session)
-    if check_api_key["result"] is False:
+    check_api_key: ResponseData = await CookiesMethods.get_user_id(request.headers.get("api-key"), session)
+    if not check_api_key.response["result"]:
         return JSONResponse(
-            content=jsonable_encoder(check_api_key),
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            content=jsonable_encoder(check_api_key.response),
+            status_code=check_api_key.status_code,
         )
 
-    tweets_data: dict = await TweetsMethods.get_all(check_api_key["user_id"], session)
-    if tweets_data["result"] is True:
-        return JSONResponse(
-            content=jsonable_encoder(tweets_data),
-            status_code=status.HTTP_200_OK,
-        )
+    tweets_data: ResponseData = await TweetsMethods.get_all(check_api_key.response["user_id"], session)
     return JSONResponse(
-        content=jsonable_encoder(tweets_data),
-        status_code=status.HTTP_404_NOT_FOUND,
+        content=jsonable_encoder(tweets_data.response),
+        status_code=tweets_data.status_code,
     )
 
 
 @router.post("", response_model=TweetResponseWithId)
-async def add_new_post(new_tweet_data: TweetDataIn, session: AsyncSession = Depends(get_async_session)):
+async def add_new_post(new_tweet_data: TweetDataIn, request: Request, session: AsyncSession = Depends(get_async_session)):
     """Create a new post.
 
     HTTP-Params:
@@ -57,22 +52,31 @@ async def add_new_post(new_tweet_data: TweetDataIn, session: AsyncSession = Depe
     Parameters:
         new_tweet_data: JSON текст поста и опциональный список
                         идентификаторов медиа файлов.
+        request: FastAPI Request object
         session: Async session
 
     Returns:
         JSONResponse: результат создания поста и идентификатор поста.
 
     """
+    new_tweet: dict = new_tweet_data.dict()
+
+    check_api_key: ResponseData = await CookiesMethods.get_user_id(request.headers.get("api-key"), session)
+    if not check_api_key.response["result"]:
+        return JSONResponse(
+            content=jsonable_encoder(check_api_key.response),
+            status_code=check_api_key.status_code,
+        )
+
+    result: ResponseData = await TweetsMethods.add(check_api_key.response["user_id"], new_tweet, session)
     return JSONResponse(
-        content=jsonable_encoder(
-            {"result": True, "tweeet_id": 1},
-        ),
-        status_code=status.HTTP_201_CREATED,
+        content=jsonable_encoder(result.response),
+        status_code=result.status_code,
     )
 
 
 @router.delete("/{post_id}", response_model=BaseResponseDataOut)
-async def delete_post(post_id: int, session: AsyncSession = Depends(get_async_session)):
+async def delete_post(post_id: int, request: Request, session: AsyncSession = Depends(get_async_session)):
     """Delete the post.
 
     HTTP-Params:
@@ -80,18 +84,27 @@ async def delete_post(post_id: int, session: AsyncSession = Depends(get_async_se
 
     Parameters:
         post_id: int
+        request: FastAPI Request object
         session: Async session
 
     Returns:
         JSONResponse: результат удаления поста
-
-    Note: make sure users delete their own posts!
     """
+    check_api_key: ResponseData = await CookiesMethods.get_user_id(request.headers.get("api-key"), session)
+    if not check_api_key.response["result"]:
+        return JSONResponse(
+            content=jsonable_encoder(check_api_key.response),
+            status_code=check_api_key.status_code,
+        )
+
+    result: ResponseData = await TweetsMethods.delete(
+        user_id=check_api_key.response["user_id"],
+        tweet_id=post_id,
+        async_session=session
+    )
     return JSONResponse(
-        content=jsonable_encoder(
-            {"result": True},
-        ),
-        status_code=status.HTTP_202_ACCEPTED,
+        content=jsonable_encoder(result.response),
+        status_code=result.status_code,
     )
 
 
