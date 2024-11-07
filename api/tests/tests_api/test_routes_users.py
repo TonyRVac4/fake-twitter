26a68c2fb799a35684b1406587f809fdf295535e
tony <technicalprogram4658@gmail.com>
2024-11-07 15:11:36 +0300

    Add additional api tests

--- api/tests/tests_api/test_routes_users.py
+++ api/tests/tests_api/test_routes_users.py
@@ -17,6 +17,57 @@ async def test_follow_user(ac: AsyncClient):
     assert request.json().get("result") is True
 
 
+async def test_can_not_follow_yourself(ac: AsyncClient):
+    """Test POST /api/users/post_id/follow can not follow yourself.
+
+    Parameters:
+        ac: AsyncClient
+    """
+    request = await ac.post(
+        url + "/{user_id}/follow".format(user_id=1),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
+    assert request.status_code == 400
+    assert request.json().get("result") is False
+    assert request.json().get("error_type") == "ValueError"
+
+
+async def test_can_not_follow_when_already_followed(ac: AsyncClient):
+    """
+    Test POST /api/users/post_id/follow
+    can not follow user that who is already followed.
+
+    Parameters:
+        ac: AsyncClient
+    """
+    request = await ac.post(
+        url + "/{user_id}/follow".format(user_id=2),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
+
+    assert request.status_code == 400
+    assert request.json().get("result") is False
+    assert request.json().get("error_type") == "UniqueViolationError"
+
+
+async def test_can_not_follow_nonexistent_user(ac: AsyncClient):
+    """
+    Test POST /api/users/post_id/follow
+    can not follow user that does not exist.
+
+    Parameters:
+        ac: AsyncClient
+    """
+    request = await ac.post(
+        url + "/{user_id}/follow".format(user_id=23),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
+
+    assert request.status_code == 404
+    assert request.json().get("result") is False
+    assert request.json().get("error_type") == "DataNotFound"
+
+
 async def test_unfollow_user(ac: AsyncClient):
     """Test DELETE /api/users/post_id/follow endpoint works.
 
@@ -30,6 +81,24 @@ async def test_unfollow_user(ac: AsyncClient):
 
     assert request.status_code == 200
     assert request.json().get("result") is True
+
+
+async def test_can_not_unfollow_when_user_is_not_followed(ac: AsyncClient):
+    """
+    Test DELETE /api/users/post_id/follow
+    can not unfollow when user is not followed.
+
+    Parameters:
+        ac: AsyncClient
+    """
+    request = await ac.delete(
+        url + "/{user_id}/follow".format(user_id=2),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
+
+    assert request.status_code == 404
+    assert request.json().get("result") is False
+    assert request.json().get("error_type") == "DataNotFound"
 
 
 async def test_get_self_profile_info(ac: AsyncClient):
@@ -49,6 +118,22 @@ async def test_get_self_profile_info(ac: AsyncClient):
 
 async def test_get_profile_info_by_id(ac: AsyncClient):
     """Test DELETE /api/users/user_id endpoint works.
+
+    Parameters:
+        ac: AsyncClient
+    """
+    request = await ac.get(
+        url + "/{user_id}".format(user_id=2),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
+
+    assert request.status_code == 200
+    assert request.json().get("result") is True
+
+
+async def test_can_not_get_user_data_of_nonexistent_user(ac: AsyncClient):
+    """Test DELETE /api/users/user_id
+    can not get user data by id that does not exist.
 
     Parameters:
         ac: AsyncClient
