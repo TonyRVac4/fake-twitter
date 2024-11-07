a495bbdefadd0ba0d9c077e4f79979ec0e6b278e
tony <technicalprogram4658@gmail.com>
2024-11-07 12:14:44 +0300

    Add: api-key header to test_routes_users.py Remove: unnecessary test data from db_setup Add: -p no:warnings parameter to unittest script

--- api/tests/tests_api/test_routes_tweet.py
+++ api/tests/tests_api/test_routes_tweet.py
@@ -32,18 +32,18 @@ async def test_create_new_post(ac: AsyncClient):
     assert request.json().get("result") is True
 
 
-# async def test_delete_post(ac: AsyncClient):
-#     """Test DELETE /api/medias/post_id endpoint works.
-#
-#     Parameters:
-#         ac: AsyncClient
-#     """
-#     request = await ac.delete(url + "/{post_id}".format(post_id=1),
-#                               headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
-#                               )
-#
-#     assert request.status_code == 202
-#     assert request.json().get("result") is True
+async def test_delete_post(ac: AsyncClient):
+    """Test DELETE /api/medias/post_id endpoint works.
+
+    Parameters:
+        ac: AsyncClient
+    """
+    request = await ac.delete(url + "/{post_id}".format(post_id=1),
+                              headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+                              )
+
+    assert request.status_code == 200
+    assert request.json().get("result") is True
 
 
 async def test_like_post(ac: AsyncClient):
--- api/tests/tests_api/test_routes_users.py
+++ api/tests/tests_api/test_routes_users.py
@@ -9,8 +9,10 @@ async def test_follow_user(ac: AsyncClient):
     Parameters:
         ac: AsyncClient
     """
-    request = await ac.post(url + "/{user_id}/follow".format(user_id=1))
-
+    request = await ac.post(
+        url + "/{user_id}/follow".format(user_id=2),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
     assert request.status_code == 201
     assert request.json().get("result") is True
 
@@ -21,9 +23,12 @@ async def test_unfollow_user(ac: AsyncClient):
     Parameters:
         ac: AsyncClient
     """
-    request = await ac.delete(url + "/{user_id}/follow".format(user_id=1))
+    request = await ac.delete(
+        url + "/{user_id}/follow".format(user_id=2),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
 
-    assert request.status_code == 202
+    assert request.status_code == 200
     assert request.json().get("result") is True
 
 
@@ -33,7 +38,10 @@ async def test_get_self_profile_info(ac: AsyncClient):
     Parameters:
         ac: AsyncClient
     """
-    request = await ac.get("".join([url, "/me"]))
+    request = await ac.get(
+        "".join([url, "/me"]),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
 
     assert request.status_code == 200
     assert request.json().get("result") is True
@@ -45,7 +53,10 @@ async def test_get_profile_info_by_id(ac: AsyncClient):
     Parameters:
         ac: AsyncClient
     """
-    request = await ac.get(url + "/{user_id}".format(user_id=1))
+    request = await ac.get(
+        url + "/{user_id}".format(user_id=2),
+        headers={"api-key": "1a2b3c4d5e6f7g8h9i0j1k2l3m4n5o6p"}
+    )
 
     assert request.status_code == 200
     assert request.json().get("result") is True
