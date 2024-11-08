d9d60e4e719ea08c756fe6ccca3b2077841fc1c0
tony <technicalprogram4658@gmail.com>
2024-11-08 14:48:07 +0300

    Add orm tests for users methods Add docstring to orm tests

--- api/tests/tests_orm/test_tweets_models.py
+++ api/tests/tests_orm/test_tweets_models.py
@@ -1,10 +1,16 @@
 from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.database_models.methods.tweets import TweetsMethods, LikesMethods  # noqa
 from database_models.tweets_orm_models import Tweets, Medias, Likes  # noqa
 
 
 async def test_get_posts_for_user(async_session: AsyncSession):
+    """Test tweets method: return list of posts for user by id.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     request = await TweetsMethods.get_posts_for_user(
         user_id=1,
         async_session=async_session
@@ -15,6 +21,11 @@ async def test_get_posts_for_user(async_session: AsyncSession):
 
 
 async def test_add_new_tweet(async_session: AsyncSession):
+    """Test tweets method: add new tweet to the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     test_data = {"tweet_data": "test data 1"}
 
     request = await TweetsMethods.add(
@@ -38,14 +49,19 @@ async def test_add_new_tweet(async_session: AsyncSession):
 
 
 async def test_delete_tweet(async_session: AsyncSession):
+    """Test tweets method: delete tweet from the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     tweet_id = 2
+    check_expr = select(Tweets).where(Tweets.id == tweet_id)
 
     async with async_session as session:
-        check_expr = select(Tweets).where(Tweets.id == tweet_id)
-        check_request = await session.execute(check_expr)
-        result = check_request.scalars().one_or_none()
-        assert result is not None
-        assert result.id == tweet_id
+        check_request_before = await session.execute(check_expr)
+        result_before = check_request_before.scalars().one_or_none()
+        assert result_before is not None
+        assert result_before.id == tweet_id
 
         request = await TweetsMethods.delete(
             user_id=1,
@@ -55,13 +71,17 @@ async def test_delete_tweet(async_session: AsyncSession):
         assert request.status_code == 200
         assert request.response.get("result") is True
 
-        check_expr = select(Tweets).where(Tweets.id == tweet_id)
-        check_request = await session.execute(check_expr)
-        result = check_request.scalars().one_or_none()
-        assert result is None
+        check_request_after = await session.execute(check_expr)
+        result_after = check_request_after.scalars().one_or_none()
+        assert result_after is None
 
 
 async def test_can_not_delete_other_person_tweet(async_session: AsyncSession):
+    """Test tweets method: can not delete other person tweet from the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     tweet_id = 3
     request = await TweetsMethods.delete(
         user_id=1,
@@ -74,6 +94,11 @@ async def test_can_not_delete_other_person_tweet(async_session: AsyncSession):
 
 
 async def test_can_not_delete_nonexistent_tweet(async_session: AsyncSession):
+    """Test tweets method: can not delete tweet from the table if it does not exist.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     tweet_id = 12
     request = await TweetsMethods.delete(
         user_id=1,
@@ -86,36 +111,79 @@ async def test_can_not_delete_nonexistent_tweet(async_session: AsyncSession):
 
 
 async def test_add_new_like(async_session: AsyncSession):
+    """Test likes method: add like to the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     user_id = 1
     tweet_id = 3
+    check_expr = select(Likes).where(and_(Likes.user_id == user_id, Likes.tweet_id == tweet_id))
+
+    async with async_session as session:
+        check_request_before = await session.execute(check_expr)
+        result_before = check_request_before.scalars().one_or_none()
+        assert result_before is None
+
+        request = await LikesMethods.add(
+            user_id=user_id,
+            tweet_id=tweet_id,
+            async_session=async_session
+        )
+        assert request.status_code == 201
+        assert request.response.get("result") is True
+
+        check_request_after = await session.execute(check_expr)
+        result_after = check_request_after.scalars().one_or_none()
+        assert result_after is not None
+        assert result_after.user_id == user_id
+        assert result_after.tweet_id == tweet_id
+
+
+async def test_can_not_add_existent_like(async_session: AsyncSession):
+    """Test likes method: can not add like to the table if it already exists.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    user_id = 1
+    tweet_id = 3
+
+    async with async_session as session:
+        check_expr = select(Likes).where(and_(Likes.user_id == user_id, Likes.tweet_id == tweet_id))
+        check_request_after = await session.execute(check_expr)
+        result_after = check_request_after.scalars().one_or_none()
+        assert result_after is not None
+        assert result_after.user_id == user_id
+        assert result_after.tweet_id == tweet_id
+
     request = await LikesMethods.add(
         user_id=user_id,
         tweet_id=tweet_id,
         async_session=async_session
     )
-
-    assert request.status_code == 201
-
-    async with async_session as session:
-        check_expr = select(Likes).where(and_(Likes.user_id == user_id, Likes.tweet_id == tweet_id))
-        check_request = await session.execute(check_expr)
-        result = check_request.scalars().one_or_none()
-    assert result is not None
-    assert result.tweet_id == tweet_id
-    assert result.user_id == user_id
+    assert request.status_code == 400
+    assert request.response.get("result") is False
 
 
 async def test_delete_like(async_session: AsyncSession):
+    """Test likes method: delete like from the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     user_id = 1
     tweet_id = 3
+    check_expr = select(Likes).where(and_(
+        Likes.user_id == user_id, Likes.tweet_id == tweet_id),
+    )
 
     async with async_session as session:
-        check_expr = select(Likes).where(and_(Likes.user_id == user_id, Likes.tweet_id == tweet_id))
-        check_request = await session.execute(check_expr)
-        result = check_request.scalars().one_or_none()
-        assert result is not None
-        assert result.tweet_id == tweet_id
-        assert result.user_id == user_id
+        check_request_before = await session.execute(check_expr)
+        result_before = check_request_before.scalars().one_or_none()
+        assert result_before is not None
+        assert result_before.tweet_id == tweet_id
+        assert result_before.user_id == user_id
 
         request = await LikesMethods.delete(
             user_id=user_id,
@@ -125,13 +193,17 @@ async def test_delete_like(async_session: AsyncSession):
         assert request.status_code == 200
         assert request.response.get("result") is True
 
-        check_expr = select(Likes).where(and_(Likes.user_id == user_id, Likes.tweet_id == tweet_id))
-        check_request = await session.execute(check_expr)
-        result = check_request.scalars().one_or_none()
-        assert result is None
+        check_request_after = await session.execute(check_expr)
+        result_after = check_request_after.scalars().one_or_none()
+        assert result_after is None
 
 
 async def test_can_not_delete_nonexistent_like(async_session: AsyncSession):
+    """Test likes method: can not delete like from the table if it does not exist.
+
+    Parameters:
+        async_session: AsyncSession
+    """
     request = await LikesMethods.delete(
         user_id=1,
         tweet_id=15,
--- api/tests/tests_orm/test_users_models.py
+++ api/tests/tests_orm/test_users_models.py
@@ -1,6 +1,219 @@
+from sqlalchemy import select, and_, func
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.database_models.methods.users import UsersMethods, FollowersMethods, CookiesMethods  # noqa
+from database_models.users_orm_models import Users, Followers, Cookies # noqa
 
 
-async def test_1(async_session: AsyncSession):
-    assert 1 == 1
+async def test_get_user_info_by_id(async_session: AsyncSession):
+    """Test users method: return info about user by id.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await UsersMethods.get_info_by_id(
+        user_id=2,
+        async_session=async_session,
+    )
+
+    assert request.status_code == 200
+    assert request.response.get("result") is True
+
+
+async def test_can_not_get_nonexistence_users_info_by_id(async_session: AsyncSession):
+    """Test users method: can not return info about user who does not exist.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await UsersMethods.get_info_by_id(
+        user_id=22,
+        async_session=async_session,
+    )
+
+    assert request.status_code == 404
+    assert request.response.get("result") is False
+    assert request.response.get("error_type") == "DataNotFound"
+
+
+async def test_follow_user(async_session: AsyncSession):
+    """Test follow method: add follow to the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    follower_id = 2
+    following_id = 1
+
+    check_expr = select(Followers).where(and_(
+        Followers.user_id == following_id,
+        Followers.follower_id == follower_id)
+    )
+
+    async with async_session as session:
+        check_request_before = await session.execute(check_expr)
+        check_result_before = check_request_before.scalars().one_or_none()
+        assert check_result_before is None
+
+        request = await FollowersMethods.add(
+            follower_id=follower_id,
+            following_id=following_id,
+            async_session=async_session,
+        )
+        assert request.status_code == 201
+        assert request.response.get("result") is True
+
+        check_request_after = await session.execute(check_expr)
+        check_result_after = check_request_after.scalars().one_or_none()
+        assert check_result_after is not None
+        assert check_result_after.user_id == following_id
+        assert check_result_after.follower_id == follower_id
+
+
+async def test_can_not_follow_yourself(async_session: AsyncSession):
+    """Test follows method: can not add follow to the table where user follows themselves.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await FollowersMethods.add(
+        follower_id=2,
+        following_id=2,
+        async_session=async_session,
+    )
+
+    assert request.status_code == 400
+    assert request.response.get("result") is False
+    assert request.response.get("error_type") == "ValueError"
+
+
+async def test_can_not_follow_already_followed_user(async_session: AsyncSession):
+    """Test follows method: can not add follow to the table if it already exists.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await FollowersMethods.add(
+        follower_id=2,
+        following_id=1,
+        async_session=async_session,
+    )
+
+    assert request.status_code == 400
+    assert request.response.get("result") is False
+    assert request.response.get("error_type") == "UniqueViolationError"
+
+
+async def test_can_not_follow_nonexistence_user(async_session: AsyncSession):
+    """Test follows method: can not add follow to the table if the user does not exist.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await FollowersMethods.add(
+        follower_id=2,
+        following_id=11,
+        async_session=async_session,
+    )
+
+    assert request.status_code == 404
+    assert request.response.get("result") is False
+    assert request.response.get("error_type") == "DataNotFound"
+
+
+async def test_unfollow_user(async_session: AsyncSession):
+    """Test follows method: delete follow from the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    follower_id = 2
+    following_id = 1
+
+    check_expr = select(Followers).where(and_(
+        Followers.user_id == following_id,
+        Followers.follower_id == follower_id)
+    )
+
+    async with async_session as session:
+        check_request_before = await session.execute(check_expr)
+        check_result_before = check_request_before.scalars().one_or_none()
+
+        assert check_result_before is not None
+        assert check_result_before.user_id == following_id
+        assert check_result_before.follower_id == follower_id
+
+        request = await FollowersMethods.delete(
+            follower_id=follower_id,
+            following_id=following_id,
+            async_session=async_session,
+        )
+        assert request.status_code == 200
+        assert request.response.get("result") is True
+
+        check_request_after = await session.execute(check_expr)
+        check_result_after = check_request_after.scalars().one_or_none()
+        assert check_result_after is None
+
+
+async def test_can_not_unfollow_not_followed_user(async_session: AsyncSession):
+    """Test follows method: can not delete follow from the table if it does not exist.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await FollowersMethods.delete(
+        follower_id=2,
+        following_id=1,
+        async_session=async_session,
+    )
+
+    assert request.status_code == 404
+    assert request.response.get("result") is False
+    assert request.response.get("error_type") == "DataNotFound"
+
+
+async def test_add_new_api_key(async_session: AsyncSession):
+    """Test cookies method: add new encrypted api-key to the table.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    key = "lk0zc7v6b5n4l1l2k1j0m9g8f7d6s5a0"
+    user_id = 2
+    check_expr = select(Cookies).where(Cookies.hash == func.crypt(key, Cookies.hash))
+
+    async with async_session as session:
+        check_request_before = await session.execute(check_expr)
+        check_result_before = check_request_before.scalars().one_or_none()
+        assert check_result_before is None
+
+        request = await CookiesMethods.add(
+            user_id=user_id,
+            api_key=key,
+            async_session=async_session,
+        )
+        assert request.status_code == 201
+        assert request.response.get("result") is True
+
+        check_request_after = await session.execute(check_expr)
+        check_result_after = check_request_after.scalars().one_or_none()
+        assert check_result_after is not None
+        assert check_result_after.user_id == user_id
+
+
+async def test_can_not_add_existing_api_key(async_session: AsyncSession):
+    """Test cookies method: can not add new encrypted api-key to the table if it already exists.
+
+    Parameters:
+        async_session: AsyncSession
+    """
+    request = await CookiesMethods.add(
+        user_id=2,
+        api_key="lk0zc7v6b5n4l1l2k1j0m9g8f7d6s5a0",
+        async_session=async_session,
+    )
+
+    assert request.status_code == 400
+    assert request.response.get("result") is False
+    assert request.response.get("error_type") == "UniqueViolationError"
