14c790a511717acff9e77ad48ea6fbb49dad062a
tony <technicalprogram4658@gmail.com>
2024-11-14 21:01:14 +0300

    Update work with env variables Update testing and linter job are now in one stage: testing
    From now on env variables they will be passed during CD job

--- api/src/config.py
+++ api/src/config.py
@@ -1,6 +1,13 @@
 import os
 
-if "DATABASE_URL" in os.environ:
-    DATABASE_URL = os.environ.get("DATABASE_URL")
+required_env_vars = ["DB_HOST", "DB_PORT", "DB_PASS", "DB_USER_NAME"]
+
+if all(var in os.environ for var in required_env_vars):
+    DATABASE_URL = "postgresql+asyncpg://{username}:{db_pass}@{host}:{port}".format(
+        host=os.environ.get("DB_HOST"),
+        port=os.environ.get("DB_PORT"),
+        db_pass=os.environ.get("DB_PASS"),
+        username=os.environ.get("DB_USER_NAME"),
+    )
 else:
-    raise EnvironmentError("DATABASE_URL variable not found")
+    raise EnvironmentError("ENV variables not found!")
