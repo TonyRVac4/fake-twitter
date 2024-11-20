e62d06f2a896913aa3713402bbcd1c4f3cfb9dd1
tony <technicalprogram4658@gmail.com>
2024-11-20 11:37:09 +0300

    Add delete_multiple class method Update util methods moved to separate class

--- api/src/utils/s3_config.py
+++ api/src/utils/s3_config.py
@@ -35,28 +35,11 @@ class S3Client:
         async with self.session.create_client("s3", **self.config) as client:
             yield client
 
-    def get_link(self, file_name: str) -> str:
-        url = "{url}/{b_name}/{f_name}".format(
-            url=self.endpoint_url,
-            b_name=self.bucket_name,
-            f_name=file_name,
-        )
-        return url
-
-    def gen_random_hash(self, filename: str) -> str:
-        characters: str = string.ascii_lowercase + string.digits + string.ascii_uppercase
-        random_str: str = ''.join(random.choice(characters) for _ in range(16))
-        file_name_with_random_str: str = filename + random_str
-
-        result = md5(file_name_with_random_str.encode()).hexdigest()
-        return result
-
     async def upload(self, file_obj: SpooledTemporaryFile, filename: str) -> ResponseData:
         hashed_filename = "{hash}.{file_ext}".format(
-            hash=self.gen_random_hash(filename),
+            hash=S3utils.gen_random_hash(filename),
             file_ext=filename.split(".")[-1],
         )
-
         try:
             async with self.get_client() as client:
                 res = await client.put_object(
@@ -67,7 +50,11 @@ class S3Client:
                 if res["ResponseMetadata"]["HTTPStatusCode"] != 200:
                     raise ClientError(operation_name="put", error_response=res)
 
-            link = self.get_link(hashed_filename)
+            link: str = S3utils.generate_link(
+                url=self.endpoint_url,
+                bucket_name=self.bucket_name,
+                filename=hashed_filename,
+            )
             result, code = {
                 "result": True,
                 "link": link,
@@ -95,6 +82,46 @@ class S3Client:
                 "error_message": err,
             }, 500
         return ResponseData(response=result, status_code=code)
+
+    async def delete_multiple(self, media_names: list) -> ResponseData:
+        try:
+            async with self.get_client() as client:
+                for name in media_names:
+                    res = await client.delete_object(Bucket=self.bucket_name, Key=name)
+                    if res["ResponseMetadata"]["HTTPStatusCode"] != 204:
+                        raise ClientError(operation_name="delete", error_response=res)
+            result, code = {"result": True}, 200
+        except ClientError as err:
+            result, code = {
+                "result": False,
+                "error_type": "ClientError",
+                "error_message": err,
+            }, 500
+        return ResponseData(response=result, status_code=code)
+
+
+class S3utils:
+    @classmethod
+    def generate_link(cls, url, bucket_name, filename: str) -> str:
+        url = "{url}/{b_name}/{f_name}".format(
+            url=url,
+            b_name=bucket_name,
+            f_name=filename,
+        )
+        return url
+
+    @classmethod
+    def get_name_from_link(cls, link: str) -> str:
+        return link.split("/")[-1]
+
+    @classmethod
+    def gen_random_hash(cls, filename: str) -> str:
+        characters: str = string.ascii_lowercase + string.digits + string.ascii_uppercase
+        random_str: str = ''.join(random.choice(characters) for _ in range(16))
+        file_name_with_random_str: str = filename + random_str
+
+        result = md5(file_name_with_random_str.encode()).hexdigest()
+        return result
 
 
 async def get_async_s3_client() -> AsyncGenerator[S3Client, None]:
