a499bc30bd9e4b5b34e68ff854826cc7be71c62b
tony <technicalprogram4658@gmail.com>
2024-12-02 11:57:41 +0300

    Fix env variables

--- api/src/config.py
+++ api/src/config.py
@@ -1,24 +1,35 @@
 import os
 
+from dotenv import load_dotenv
+
 required_db_env_vars = ["DB_HOST", "DB_PORT", "DB_PASS", "DB_USER_NAME", "DB_NAME"]
 required_s3_env_vars = ["S3_ACCESS_KEY", "S3_SECRET_KEY", "S3_URL", "S3_BUCKET_NANE"]
 
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
+load_dotenv(dotenv_path=env_path)
+
+
 if all(var in os.environ for var in required_db_env_vars):
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_USER_NAME = os.getenv("DB_USER_NAME")
+    DB_NAME = os.getenv("DB_NAME")
     DATABASE_URL = "{driver}://{username}:{db_pass}@{host}:{port}/{db_name}".format(
         driver="postgresql+asyncpg",
-        host=os.environ.get("DB_HOST"),
-        port=os.environ.get("DB_PORT"),
-        db_pass=os.environ.get("DB_PASS"),
-        username=os.environ.get("DB_USER_NAME"),
-        db_name=os.environ.get("DB_NAME"),
+        host=DB_HOST,
+        port=DB_PORT,
+        db_pass=DB_PASS,
+        username=DB_USER_NAME,
+        db_name=DB_NAME,
     )
 else:
     raise EnvironmentError("Database ENV variables not found!")
 
 if all(var in os.environ for var in required_s3_env_vars):
-    S3_ACCESS_KEY = os.environ.get("S3_ACCESS_KEY")
-    S3_SECRET_KEY = os.environ.get("S3_SECRET_KEY")
-    S3_URL = os.environ.get("S3_URL")
-    S3_BUCKET_NANE = os.environ.get("S3_BUCKET_NANE")
+    S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
+    S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
+    S3_URL = os.getenv("S3_URL")
+    S3_BUCKET_NANE = os.getenv("S3_BUCKET_NANE")
 else:
     raise EnvironmentError("S3 ENV variables not found!")
