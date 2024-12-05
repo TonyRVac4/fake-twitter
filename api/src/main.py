e291522803d619dd64bc697dce727b79ebac68a8
tony <technicalprogram4658@gmail.com>
2024-12-05 14:00:15 +0300

    Add CORS middleware

--- api/src/main.py
+++ api/src/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import Depends, FastAPI, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from middleware import api_key_check_dependency
 from routers import medias, tweets, users
@@ -12,9 +13,21 @@ app = FastAPI(
     dependencies=[Depends(api_key_check_dependency)],
 )
 
+origins = [
+    "http://localhost",
+    "http://195.133.73.158",
+]
+
 app.include_router(tweets.router)
 app.include_router(users.router)
 app.include_router(medias.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api")
