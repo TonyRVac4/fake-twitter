c16b64b87f18c931ccb12b3f06afb4fa86b18eff
tony <technicalprogram4658@gmail.com>
2024-12-11 14:07:22 +0300

    Add schemas for GET /api/tweets endpoint response_model

--- api/src/schemas.py
+++ api/src/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, ConfigDict
+from typing import List
 
 
 class BaseResponseDataOut(BaseModel):
@@ -16,6 +17,13 @@ class UserBaseData(BaseModel):
     name: str
 
 
+class UserBaseDataV2(BaseModel):
+    """Class with basic user info (id, name)."""
+
+    user_id: int
+    name: str
+
+
 class TweetDataIn(BaseModel):
     """Class for validation incoming new post data."""
 
@@ -27,6 +35,22 @@ class TweetResponseWithId(BaseResponseDataOut):
     """Class extends basic response with tweed_id."""
 
     tweet_id: int
+
+
+class TweetFullData(BaseModel):
+    """Class response with full info about tweet."""
+
+    id: int
+    content: str
+    attachments: List[str]
+    author: UserBaseData
+    likes: List[UserBaseDataV2]
+
+
+class TweetsListDataOut(BaseResponseDataOut):
+    """Class extends basic response with tweets list."""
+
+    tweets: List[TweetFullData]
 
 
 class MediaUploadResponseDataWithId(BaseResponseDataOut):
