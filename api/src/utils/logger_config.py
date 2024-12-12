51b3aa084a7a6ee7cbdb02648df2deb7daf92ac0
tony <technicalprogram4658@gmail.com>
2024-12-12 12:05:50 +0300

    Fix logger_config and gitignore

--- api/src/utils/logger_config.py
+++ api/src/utils/logger_config.py
@@ -3,6 +3,10 @@ import os
 
 os.chdir(os.path.dirname(os.path.dirname(__file__)))
 
+log_dir = "logs"
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
 root_logger = logging.getLogger()
 api_logger = logging.getLogger("api")
 orm_logger = logging.getLogger("orm")
@@ -10,7 +14,7 @@ s3_logger = logging.getLogger("s3")
 
 root_logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("logs/logs.log", mode="w")
+file_handler = logging.FileHandler("logs/logs.log", mode="a")
 console_handler = logging.StreamHandler()
 formatter = logging.Formatter(
     "%(name)s | %(asctime)s | %(levelname)s | %(message)s",  # noqa: WPS323
